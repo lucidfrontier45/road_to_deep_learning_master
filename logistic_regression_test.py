7aea243c0314e21a1ecbafd56ca22016a3138e55
Shiqiao Du <s.du@freebit.net>
2015-07-17 17:14:49 +0900

    update test scripts

--- logistic_regression_test.py
+++ logistic_regression_test.py
@@ -12,11 +12,11 @@ y = iris.target
 n_dim = X.shape[1]
 n_classes = len(set(y))
 
-model1 = SGDLogisticRegression(n_dim, n_classes, batch_size=30, tol=1e-7,
-                               n_iter=100000, report=1000, C=0.1).fit(X, y)
+model1 = SGDLogisticRegression(n_dim, n_classes, batch_size=50, tol=1e-7, lr=0.001,
+                               n_iter=100000, report=1000, C=0.5).fit(X, y)
 print(model1.W_)
 print(model1.score(X, y))
 
-model2 = BatchLogisticRegression(n_dim, n_classes, tol=1e-7, n_iter=1000000, C=0.1, method="CG").fit(X, y)
+model2 = BatchLogisticRegression(n_dim, n_classes, tol=1e-7, n_iter=1000000, C=0.5, method="CG").fit(X, y)
 print(model2.W_)
 print(model2.score(X, y))
