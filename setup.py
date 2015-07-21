8da57aac386b228d2076b6ea11d7eece195754f7
Shiqiao Du <s.du@freebit.net>
2015-07-21 12:29:36 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='road_to_deep_learning',
+    version='0.1',
+    packages=['road_to_deep_learning', 'road_to_deep_learning.naive', 'road_to_deep_learning.utils'],
+    url='',
+    license='MIT',
+    author='Shiqiao Du',
+    author_email='lucidfrontier.45@gmail.com',
+    description=''
+)
